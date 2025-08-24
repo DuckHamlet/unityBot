2a7b61d0b7da00be58d083d0e3b3171af54d588f
Andrew <tjey@inbox.ru>
2025-08-24 16:38:10 +0300

    Initial

---
+++ Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    float MaxSpeed = 3.5f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Returns a float from -1.0 to +1.0
+       
+        Vector3 pos = transform.position ;
+        // Input.GetAxis("Vertical")
+        pos.y += MaxSpeed * Time.deltaTime ;
+        transform.position = pos ;
+    }
+}
